abe871dcd4898c466b2322ac21a54d85ecfe5a85
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2024-07-18 16:15:51 +0200

    Add Game page css

--- client/src/Components/Answers.jsx
+++ client/src/Components/Answers.jsx
@@ -20,11 +20,10 @@ export default function Answers ({answers, progress, correctAnswer, onProgress, 
   return (
     <div className="answers-container">
       <div className="answers-content">
-        <div className="answerA" id='A' onClick={handleAnswer}>A: {answers.answerA}</div>
-        <div className="answerB" id='B' onClick={handleAnswer}>B: {answers.answerB}</div>
-        <br></br>
-        <div className="answerC" id='C' onClick={handleAnswer}>C: {answers.answerC}</div>
-        <div className="answerD" id='D' onClick={handleAnswer}>D: {answers.answerD}</div>
+        <div className="answerA" onClick={handleAnswer}><div className="letter">A: </div> <div id='A'>{answers.answerA}</div></div>
+        <div className="answerB" onClick={handleAnswer}><div className="letter">B: </div> <div id='B'>{answers.answerB}</div></div>
+        <div className="answerC" onClick={handleAnswer}><div className="letter">C: </div> <div id='C'>{answers.answerC}</div></div>
+        <div className="answerD" onClick={handleAnswer}><div className="letter">D: </div> <div id='D'>{answers.answerD}</div></div>
       </div>
     </div>
   )
--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -3,8 +3,10 @@ import { Link, Outlet } from "react-router-dom"
 export default function Layout () {
   return (
     <>
-      <Link to={'/login'}>login page</Link> <br />
-      <Link to={'/register'}>Register page</Link>
+      <div style={{visibility: 'hidden'}}>
+        <Link to={'/login'}>login page</Link> <br />
+        <Link to={'/register'}>Register page</Link>
+      </div>
       <Outlet/>
     </>
   )
