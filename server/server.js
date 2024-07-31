74df1c5529f45a42d2c1ac21aaee9461329d02b4
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2024-07-31 14:38:44 +0200

    Refactor /api/questions-by-id/:id endpoint

--- server/server.js
+++ server/server.js
@@ -132,7 +132,7 @@ app.get('/api/questions-all', async (req, res) => {
 });
 
 app.get('/api/questions-by-id/:id', async (req, res) => {
-  res.json(await Question.find({user: req.params.id}).populate('user')); 
+  res.json(await Question.find({user: new mongoose.Types.ObjectId(req.params.id)}).populate('user')); 
 });
 
 app.post('/api/question', async (req, res) => {
