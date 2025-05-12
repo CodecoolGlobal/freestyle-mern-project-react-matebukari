8540be52e55bb3841d6c00e69918f7f5e90a4e9a
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-12 14:26:42 +0200

    Change import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/frontend'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout/Layout.jsx'
