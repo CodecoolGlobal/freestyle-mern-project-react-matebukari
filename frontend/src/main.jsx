cd1cfb837addcd57b6370577e4e43cb477b7f8bd
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-12 21:52:40 +0200

    Add soundProvider to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,41 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Layout from './Layout/Layout.jsx'
 import LoginPage from './Pages/LoginPage/LoginPage.jsx'
 import RegisterPage from './Pages/RegisterPage/RegisterPage.jsx'
 import GamePage from './Pages/GamePage/GamePage.jsx'
 import MyQuestions from "./Pages/MyQuestions/MyQuestions.jsx"
+import {SoundProvider} from "./Context/SoundProvider.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children: [
-      {
+    {
         path: '/',
-        element: <LoginPage/>,
-      },
-      {
-        path: '/register',
-        element: <RegisterPage/>
-      },
-      {
-        path: '/game',
-        element: <GamePage/>
-      },
-      {
-        path: '/questions-by-user/:id',
-        element: <MyQuestions/>
-      }
+        element: <Layout/>,
+        children: [
+            {
+                path: '/',
+                element: <LoginPage/>,
+            },
+            {
+                path: '/register',
+                element: <RegisterPage/>
+            },
+            {
+                path: '/game',
+                element: <GamePage/>
+            },
+            {
+                path: '/questions-by-user/:id',
+                element: <MyQuestions/>
+            }
 
-    ]
-  }
+        ]
+    }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <SoundProvider>
+            <RouterProvider router={router}/>
+        </SoundProvider>
+    </React.StrictMode>,
 )
