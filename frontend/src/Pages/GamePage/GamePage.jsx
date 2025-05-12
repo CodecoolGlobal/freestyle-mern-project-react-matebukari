e1a6bda1cd2ebd0fac65ac0bf9a01c60f5911f97
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-12 21:52:01 +0200

    Refactor GamePage component

--- frontend/src/Pages/GamePage/GamePage.jsx
+++ frontend/src/Pages/GamePage/GamePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import AddQuestionModal from "../../Components/AddQuestionModal/AddQuestionModal.jsx";
 import GameQuestion from "../../Components/GameQuestion/GameQuestion.jsx";
 import Answers from "../../Components/Answers/Answers.jsx";
@@ -11,147 +11,150 @@ import LeaderBoardModal from "../../Components/LeaderBoardModal/LeaderBoardModal
 import AllQuestionsModal from "../../Components/AllQuestionsModal/AllQuestionsModal.jsx";
 import CheckpointModal from "../../Components/CheckPointModal/CheckpointModal.jsx";
 import LogOutModal from "../../Components/LogOutModal/LogOutModal.jsx";
-import useSound from 'use-sound';
-import questionMusic from "../../sounds/questionSound.mp3"
-import checkpointMusic from "../../sounds/checkpointSound.mp3"
+import {useSoundContext} from "../../Context/SoundProvider.jsx";
 
 
 const updateUser = async (user) => {
-  const res = await fetch(`/api/user/${user._id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
-  return await res.json();
+    const res = await fetch(`/api/user/${user._id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    });
+    return await res.json();
 };
 
 export default function GamePage() {
-  const userJSON = localStorage.getItem("user");
-  const userData = JSON.parse(userJSON);
-  const [gameStart, setGameStart] = useState(false);
-  const [showQuestionModal, setShowQuestionModal] = useState(false);
-  const [questions, setQuestions] = useState(null);
-  const [progress, setProgress] = useState(0);
-  const [isWon, setIsWon] = useState(false);
-  const [isLost, setIsLost] = useState(false);
-  const [score, setScore] = useState(0);
-  const [prices, setPrices] = useState([]);
-  const [openDropDown, setOpenDropDown] = useState(false);
-  const [showLeaderBoard, setShowLeaderBoard] = useState(false);
-  const [showAllQuestion, setShowAllQuestion] = useState(false);
-  const [showCheckpoint, setShowCheckpoint] = useState(false);
-  const [showValidLogout, setShowValidLogout] = useState(false)
-  const [questionSound, {stop}] = useSound(questionMusic);
-  const [checkpointSound] = useSound(checkpointMusic);
+    const userJSON = localStorage.getItem("user");
+    const userData = JSON.parse(userJSON);
+    const [gameStart, setGameStart] = useState(false);
+    const [showQuestionModal, setShowQuestionModal] = useState(false);
+    const [questions, setQuestions] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [isWon, setIsWon] = useState(false);
+    const [isLost, setIsLost] = useState(false);
+    const [score, setScore] = useState(0);
+    const [prices, setPrices] = useState([]);
+    const [openDropDown, setOpenDropDown] = useState(false);
+    const [showLeaderBoard, setShowLeaderBoard] = useState(false);
+    const [showAllQuestion, setShowAllQuestion] = useState(false);
+    const [showCheckpoint, setShowCheckpoint] = useState(false);
+    const [showValidLogout, setShowValidLogout] = useState(false);
+    const [savedScore, setSavedScore] = useState(0);
+    const {playCheckpoint, playQuestion, stopAllSound} = useSoundContext();
 
+    useEffect(() => {
+        if (questions !== null) {
+            stopAllSound();
+            setScore(prices[progress])
+            if ((progress === 0 ? 1 : progress + 1) % (questions.length / 3) === 0) {
+                playCheckpoint();
+            } else {
+                playQuestion();
+            }
+        }
 
-  useEffect(()=> {
-    
-    if (questions !== null){
-      questionSound();
-      if((progress + 1) % (questions.length / 3) === 0 ) {
-        setScore(prices[progress])
-      }
-      if ((progress === 0 ? 1 : progress) % (questions.length / 3) === 0) {
-        setShowCheckpoint(true);
-        checkpointSound();
-      }
+    }, [progress, prices.length]);
+
+    function onLoseReset() {
+        setGameStart(false);
+        setIsLost(false);
+        setProgress(0);
+        setIsWon(false);
+        updateUser({
+            ...userData,
+            score: (userData.score + score),
+        });
     }
 
-  }, [ progress]);
+    function onWinReset() {
+        setGameStart(false);
+        setIsLost(false);
+        setProgress(0);
+        setIsWon(false);
+        setShowCheckpoint(false);
+        updateUser({
+            ...userData,
+            score: (userData.score + score),
+        });
+    }
 
-  function onLoseReset(){
-    setGameStart(false);
-    setIsLost(false);
-    setProgress(0);
-    setIsWon(false);
-  }
+    async function handleGameStart() {
+        const response = await fetch('/api/questions-ingame');
+        const result = await response.json();
+        setQuestions(result.questions);
+        setGameStart(true);
+        stopAllSound();
+    }
 
-  const newUser = {
-    ...userData,
-    score: (userData.score + score),
-  }
+    function handleAddQuestion() {
+        setShowQuestionModal(true);
+    }
 
-  function onWinReset () {
-    setGameStart(false);
-    setIsLost(false);
-    setProgress(0);
-    setIsWon(false);
-    updateUser(newUser);
-  }
+    function handleShowLeaderBoard() {
+        setShowLeaderBoard(true);
+    }
 
-  async function handleGameStart() {
-    const response = await fetch('/api/questions-ingame');
-    const result = await response.json();
-    setQuestions(result.questions);
-    setGameStart(true);
-    questionSound();
-  }
+    function handleShowQuestions() {
+        setShowAllQuestion(true);
+    }
 
-  function handleAddQuestion () {
-    setShowQuestionModal(true);
-  }
+    function handleLogOut() {
+        setShowValidLogout(true);
+    }
 
-  function handleShowLeaderBoard () {
-    setShowLeaderBoard(true);
-  }
+    function handleContinue() {
+        if((progress === 0 ? 1 : progress + 1) % (questions.length / 3) === 0){
+            setSavedScore(prices[progress]);
+        }
+        setShowCheckpoint(false);
+        setProgress(prev => prev + 1);
+    }
 
-  function handleShowQuestions () {
-    setShowAllQuestion(true);
-  }
+    return (
+        <div className="game-root">
+            <nav className="nav-bar">
+                {openDropDown && (
+                    <UserDropDown
+                        onAddQuestion={handleAddQuestion}
+                        onShowQuestions={handleShowQuestions}
+                        onShowLeaderBoard={handleShowLeaderBoard}
+                        onLogOut={handleLogOut}/>)}
+                <div className="user" onClick={() => setOpenDropDown((prev) => !prev)}>{userData.name}</div>
+            </nav>
+            {showValidLogout && <LogOutModal toggleModal={setShowValidLogout}/>}
+            {showLeaderBoard && <LeaderBoardModal toggleModal={setShowLeaderBoard}/>}
+            {showAllQuestion && <AllQuestionsModal toggleModal={setShowAllQuestion}/>}
+            {!gameStart && <>
+                <div className="startBtn-container">
+                    <div className="startBtn-center">
+                        <button className="startBtn" onClick={handleGameStart}>Start</button>
+                    </div>
+                </div>
+                {showQuestionModal && <AddQuestionModal user={userData} toggleModal={setShowQuestionModal}/>}
 
-  function handleLogOut () {
-    setShowValidLogout(true);
-  }
-
-  return (
-    <div className="game-root">
-      <nav className="nav-bar">
-        {/* onMyQuestion={} onAllQuestion={} */}
-        {/* <img src="./d*ckpic.jpg" alt="d*ckpic" /> */}
-        {openDropDown && (
-        <UserDropDown 
-          onAddQuestion={handleAddQuestion} 
-          onShowQuestions={handleShowQuestions} 
-          onShowLeaderBoard={handleShowLeaderBoard} 
-          onLogOut={handleLogOut}/>)}
-      <div className="user"onClick={() => setOpenDropDown((prev) => !prev)}>{userData.name}</div>
-      </nav>
-    {showValidLogout && <LogOutModal toggleModal={setShowValidLogout}/>}
-    {showLeaderBoard && <LeaderBoardModal toggleModal={setShowLeaderBoard}/>}
-    {showAllQuestion && <AllQuestionsModal toggleModal={setShowAllQuestion}/>}
-    {!gameStart && <>
-      <div className="startBtn-container">
-        <div className="startBtn-center">
-          <button className="startBtn" onClick={handleGameStart}>Start</button>
+            </>}
+            {gameStart &&
+                <>
+                    {questions && <ProgressList maxQuestions={questions.length} prices={prices} onPriceSet={setPrices}
+                                                progress={progress}/>}
+                    {questions && <GameQuestion questions={questions} progress={progress}/>}
+                    {questions && <Answers
+                        answers={questions[progress].answers}
+                        progress={progress}
+                        onWinning={setIsWon}
+                        onLosing={setIsLost}
+                        maxQuestions={questions.length}
+                        correctAnswer={questions[progress].correctAnswer}
+                        difficulty={questions[progress].difficulty}
+                        onCorrectAnswer={setShowCheckpoint}/>}
+                    {isWon && <WinModal onReset={onWinReset} score={score}/>}
+                    {isLost && <LosingModal savedScore={savedScore} onReset={onLoseReset}/>}
+                    {showCheckpoint && <CheckpointModal score={score} onContinue={handleContinue} onQuit={onWinReset}/>}
+                </>
+            }
         </div>
-      </div>
-      {showQuestionModal && <AddQuestionModal user={userData} toggleModal={setShowQuestionModal}/>}
-      
-    </>}
-    {gameStart && 
-    <>
-      {questions && <ProgressList maxQuestions={questions.length} prices={prices} onPriceSet={setPrices} progress={progress}/>}
-      {questions && <GameQuestion questions={questions} progress={progress}/>}
-      {questions && <Answers 
-        answers={questions[progress].answers} 
-        progress={progress} 
-        onProgress={setProgress} 
-        onWinning={setIsWon}
-        onLosing={setIsLost}
-        maxQuestions={questions.length}
-        correctAnswer={questions[progress].correctAnswer}
-        difficulty={questions[progress].difficulty}/>}
-      {/* <button onClick={() => setProgress((prev) => prev - 1)}>Prev</button>
-      <button onClick={() => setProgress((prev) => prev + 1)}>Next</button> */}
-      {isWon && <WinModal onReset={onWinReset} score={score}/>}
-      {isLost && <LosingModal onReset={onLoseReset}/>}
-      {showCheckpoint && ((progress === 0 ? 1 : progress) % (questions.length / 3) === 0) && <CheckpointModal onClose={setShowCheckpoint} score={score} onQuit={onWinReset}/>}
-    </>
-    }
-    </div>
 
-  );
+    );
 }
