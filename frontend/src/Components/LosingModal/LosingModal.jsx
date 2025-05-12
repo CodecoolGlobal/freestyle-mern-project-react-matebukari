62b40c63e22499c2699d75d5e6b5bda580de935d
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-12 21:52:19 +0200

    Refactor LosingModal component

--- frontend/src/Components/LosingModal/LosingModal.jsx
+++ frontend/src/Components/LosingModal/LosingModal.jsx
@@ -1,11 +1,11 @@
 import "./loseModal.css"
 
-export default function LosingModal({onReset}) {
+export default function LosingModal({onReset, savedScore}) {
   return (
     <div className="losing-modal-overlay">
       <div className="losing-modal-container">
         <div className="losing-modal-content">
-          <h1>Sadly, you lost the game </h1> 
+          <h1>Sadly, you lost the game, but received {`${new Intl.NumberFormat('en-HU').format(savedScore)} Ft`}</h1>
           <button onClick={onReset}> RESTART</button>
         </div>
       </div>
