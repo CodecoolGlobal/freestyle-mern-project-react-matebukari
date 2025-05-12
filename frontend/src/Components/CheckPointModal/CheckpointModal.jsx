583564521d2d6ce91a27b0a56ffd7692203a6ea2
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-12 21:51:46 +0200

    Refactor CheckpointModal component

--- frontend/src/Components/CheckPointModal/CheckpointModal.jsx
+++ frontend/src/Components/CheckPointModal/CheckpointModal.jsx
@@ -1,17 +1,14 @@
 import "./checkpointModal.css"
 
-export default function CheckpointModal ({ onClose, onQuit, score}) {
+export default function CheckpointModal ({ onQuit, score, onContinue}) {
 
-  function closeModal () {
-    onClose(false);
-  }
 
   return (
     <div className="checkpoint-modal-overlay">
       <div className="checkpoint-modal-container">
         <div className="checkpoint-modal-content">
           <h1>Do you want to end your game now and win {`${new Intl.NumberFormat('en-HU').format(score)} Ft`}</h1> 
-          <button onClick={closeModal}>No i want to continue</button> <br />
+          <button onClick={onContinue}>No i want to continue</button> <br />
           <button onClick={onQuit}>Yes quit now</button>
         </div>
       </div>
